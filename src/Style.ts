57f6313f1ad1627d892d1b1495bbdd2e61ed6bf0
JokerOo <perfecttango@gmail.com>
2020-03-29 23:24:49 +0800

    first

---
+++ src/Style.ts
@@ -0,0 +1,74 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import * as vscode from 'vscode';
+
+const jsName = 'bootstrap-window.js';
+const pkg = require("./../package.json");
+
+class Style {
+    jsPath: string = "";
+    extName: string = "extension-am9rZXJiYWNrZ3JvdW5k";
+
+    constructor() {
+        let fileName: string | undefined = require.main?.filename;
+        if (fileName) {
+            this.jsPath = path.join(path.dirname(fileName), jsName);
+            vscode.window.showInformationMessage(this.jsPath);
+        }
+        this.extName = "extension-am9rZXJiYWNrZ3JvdW5k"
+    }
+
+    install() {
+        let addJs: string = this.getJs();
+        let js: string = this.getContent(this.jsPath);
+        let originJs: string = this.clearContent(js);
+        this.saveContent(this.jsPath, originJs + addJs);
+        return true;
+    }
+
+    uninstall() {
+        try {
+            const js = this.getContent(this.jsPath);
+            const originJs = this.clearContent(js);
+            this.saveContent(this.jsPath, originJs);
+            return true;
+        }
+        catch (e) {
+            return false;
+        }
+    }
+
+    getJs(): string {
+        const opacity = 0.95;
+        let imagesJs = '"D:\\Pictures\\201008094903-18956.jpg"'.replace(/\\/g, '/');
+        return `
+/*${this.extName}-start*/
+/*${this.extName}.ver.${pkg.version}*/
+window.onload = function() {
+    const image = ${imagesJs}
+    const defaultOpacity = ${opacity}
+
+    document.body.style.opacity = defaultOpacity
+    document.body.style.backgroundSize = "cover"
+    document.body.style.backgroundRepeat = "no-repeat"
+    document.body.style.backgroundImage = "url('" + image + "')"
+}
+/*${this.extName}-end*/`.replace(/\s*$/, '');
+    }
+
+    getContent(contentPath: string) {
+        return fs.readFileSync(contentPath, 'utf-8');
+    }
+
+    saveContent(contentPath: string, content: any) {
+        fs.writeFileSync(contentPath, content, 'utf-8');
+    }
+
+    clearContent(content: string) {
+        let re = new RegExp("\\/\\*" + this.extName + "-start\\*\\/[\\s\\S]*?\\/\\*" + this.extName + "-end\\*" + "\\/", "g");
+        content = content.replace(re, '');
+        content = content.replace(/\s*$/, '');
+        return content;
+    }
+}
+export default new Style();
---
+++ src/extension.ts
@@ -0,0 +1,37 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import * as path from 'path';
+import Style from './Style';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "jokerbackground" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let star = vscode.commands.registerCommand('extension.jokerstar', () => {
+		Style.install();
+		vscode.commands.executeCommand('workbench.action.reloadWindow');
+	});
+	let stop = vscode.commands.registerCommand('extension.jokerstop', () => {
+		Style.uninstall();
+		vscode.commands.executeCommand('workbench.action.reloadWindow');
+	});
+	let open = vscode.commands.registerCommand('extension.openfile', () => {
+		Style.install();
+		vscode.commands.executeCommand('workbench.action.reloadWindow');
+	});
+
+	context.subscriptions.push(star);
+	context.subscriptions.push(stop);
+	context.subscriptions.push(open);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() { }
